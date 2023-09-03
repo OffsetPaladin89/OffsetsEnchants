a20be63eaa2067909923ccf78df1e63793e38e01
QOLModDev <offsetpaladin89@hotmail.com>
2023-09-03 13:17:22 -0700

    Created Bukkit Project
    Bukkit project created

---
+++ src/main/java/me/offsetpaladin89/offsetsenchants/OffsetsEnchants.java
@@ -0,0 +1,17 @@
+package me.offsetpaladin89.offsetsenchants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OffsetsEnchants extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
